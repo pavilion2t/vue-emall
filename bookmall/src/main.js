c0819afb49a726763647d16a751be0d83ad164a8
Judy <judy.zhu@bindo.com>
2018-04-15 12:48:01 +0800

    添加路由和首页

--- bookmall/src/main.js
+++ bookmall/src/main.js
@@ -1,13 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
 
-Vue.config.productionTip = false
-
+Vue.use(VueRouter)  
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {path: '/', component: IndexPage}
+  ]
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  router,
+  template: '<Layout/>',
+  components: { Layout }
 })
